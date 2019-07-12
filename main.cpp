aa6303b7d07e57c8e0f3548a1a8c134676c6faca
pt207 <47851246+Hay333@users.noreply.github.com>
2019-07-12 10:01:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,48 @@
+
 #include <iostream>
-#include <iomanip>
-#define N 10
 using namespace std;
 
-template <typename T> void massivprint(T *mass, int b);
-template <typename T> void sort(T *mass, unsigned len);
+template <typename T1, typename T2> class DoubleBox
+{
+    T1 data1;
+    T2 data2;
+public:
+    DoubleBox(): data1(0), data2(0){};// конструктор по умолч.
+    DoubleBox(const T1 value1, const T2 value2):data1(value1), data2(value2) {};// конструктор инициализации 
+    T1 get1() const;
+    T2 get2() const;
+    void get() const;//метод get
+    void set(const T1 value1, const T2 value2);//метод set
+};
+
+template <typename T1, typename T2> void DoubleBox<T1, T2>::get() const
+{
+	cout << data1 << endl << data2 << endl;
+}
+
+template <typename T1, typename T2> T1 DoubleBox<T1, T2>::get1() const
+{
+    return data1;
+}
+
+template <typename T1, typename T2> T2 DoubleBox<T1, T2>::get2() const
+{
+    return data2;
+}
+
+template <typename T1, typename T2> void DoubleBox<T1, T2>::set(const T1 value1, const T2 value2)
+{
+    data1 = value1;
+    data2 = value2;
+}
 
 int main(int argc, char **argv)
 {
-	// int массив
-	int massiv[N] = { 74, 56, 81, 19, 63, 9, 237, 34, 7, 47 };
-	massivprint(massiv, N);
-	sort(massiv, N);
-	massivprint(massiv, N);
-	cout << endl;
-	// double массив
-	double massiv1[N] = { 56.3554, 78.345354, 2.82222, 19.3425, 65.0, 9.34522, 456.89422, 56.78922, 8.4};
-	massivprint(massiv1, N);
-	sort(massiv1, N);
-	massivprint(massiv1, N);
-	cout << endl;
-	// float массив
-	float massiv2[N] = {344.0967, 65.76, 66.345, 8.645, 7.6, 344.658, 45.876, 1.2345, 78.353, 98.04};
-	massivprint(massiv2, N);
-	sort(massiv2, N);
-	massivprint(massiv2, N);
-	cout << endl;
-	// char массив
-	char massiv3[N] = {'w','z', 'u', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
-	massivprint(massiv3, N);
-	sort(massiv3, N);
-	massivprint(massiv3, N);
-	cout << endl;
+    DoubleBox<int, double> a(5,6.2342);
+    cout << a.get1() << endl;
+    cout << a.get2() << endl;
+    a.set(10, 12.23423);
+    cout << a.get1() << endl;
+    cout << a.get2() << endl;
 	return 0;
-}
-
-template <typename T> void massivprint(T *mass, int b)
-{
-	for (int i = 0; i < b; i++)
-	{
-		cout << " " << mass[i];
-	}
-	cout << endl;
-}
-template <typename T> void sort(T*mass, unsigned len)
-{
-    for(int i = 0; i < len - 1; i++) {
-    for(int j=0;
-	j<len-1;j++)
-    {
-       if (mass[j] > mass[j + 1]) { // меняем элементы местами
-               int  temp = mass[j];
-                mass[j] = mass[j + 1];
-                mass[j + 1] = temp;
-            } 
-    }
-}
 }
