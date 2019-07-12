22b9b826df3bd5a6b747babf359db3834e1ef9f3
pt207 <47851246+Hay333@users.noreply.github.com>
2019-07-12 10:00:31 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,103 @@
 #include <iostream>
 #include <iomanip>
-#define N 10
+#define N 5
 using namespace std;
 
 template <typename T> void massivprint(T *mass, int b);
 template <typename T> void sort(T *mass, unsigned len);
+template <typename T> void swap(T *pa, T *pb);
+
+
+class  Rectangle
+{
+    int h, w, s;
+public:
+    Rectangle() : h(0), w(0), s(0) {};
+    Rectangle(int a, int b) {
+        h = a;
+        w = b;
+        s = h * w;
+    }
+    friend ostream& operator<<(ostream& outputStream, const Rectangle a);
+    bool operator>(const Rectangle s);
+    bool operator<(const Rectangle s);
+    bool operator==(const Rectangle s);
+    bool operator>=(const Rectangle s);
+    bool operator<=(const Rectangle s);
+};
+
+ostream& operator<<(ostream& outputStream, const Rectangle a)
+{
+    return outputStream << a.s;
+}
+
+bool Rectangle::operator>(const Rectangle s)
+{
+    if (this->s > s.s)
+        return 1;
+    return 0;
+}
+
+bool Rectangle::operator<(const Rectangle s)
+{
+    if (this->s < s.s)
+        return 1;
+    return 0;
+}
+
+bool Rectangle::operator==(const Rectangle s)
+{
+    if (this->s == s.s)
+        return 1;
+    return 0;
+}
+
+bool Rectangle::operator>=(const Rectangle s)
+{
+    if (this->s >= s.s)
+        return 1;
+    return 0;
+}
+
+bool Rectangle::operator<=(const Rectangle s)
+{
+    if (this->s <= s.s)
+        return 1;
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
-	// int массив
-	int massiv[N] = { 74, 56, 81, 19, 63, 9, 237, 34, 7, 47 };
-	massivprint(massiv, N);
-	sort(massiv, N);
-	massivprint(massiv, N);
-	cout << endl;
-	// double массив
-	double massiv1[N] = { 56.3554, 78.345354, 2.82222, 19.3425, 65.0, 9.34522, 456.89422, 56.78922, 8.4};
-	massivprint(massiv1, N);
-	sort(massiv1, N);
-	massivprint(massiv1, N);
-	cout << endl;
-	// float массив
-	float massiv2[N] = {344.0967, 65.76, 66.345, 8.645, 7.6, 344.658, 45.876, 1.2345, 78.353, 98.04};
-	massivprint(massiv2, N);
-	sort(massiv2, N);
-	massivprint(massiv2, N);
-	cout << endl;
-	// char массив
-	char massiv3[N] = {'w','z', 'u', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
-	massivprint(massiv3, N);
-	sort(massiv3, N);
-	massivprint(massiv3, N);
-	cout << endl;
-	return 0;
+    Rectangle b[N] = { {5,4}, {4, 4}, {7, 8}, {6, 9}, {8, 9}, };
+    massivprint(b, N);
+    sort(b, N);
+    massivprint(b, N);
+    return 0;
 }
 
 template <typename T> void massivprint(T *mass, int b)
 {
-	for (int i = 0; i < b; i++)
-	{
-		cout << " " << mass[i];
-	}
-	cout << endl;
-}
-template <typename T> void sort(T*mass, unsigned len)
-{
-    for(int i = 0; i < len - 1; i++) {
-    for(int j=0;
-	j<len-1;j++)
-    {
-       if (mass[j] > mass[j + 1]) { // меняем элементы местами
-               int  temp = mass[j];
-                mass[j] = mass[j + 1];
-                mass[j + 1] = temp;
-            } 
+    for (int i = 0; i < b; i++) {
+        cout << "  " << mass[i];
     }
+    cout << endl;
 }
+template <typename T> void swap(T *pa, T *pb)
+{
+	T temp = *pa;
+	*pa = *pb;
+	*pb = temp;
 }
+
+template <typename T> void sort(T *mass, unsigned len)
+
+    {
+        for(int i = 0; i < len - 1; i++) {
+             for(int j = 0; j < len - 1; j++) {
+                  if (mass[j] > mass[j+1]) {
+                  
+ swap(mass,mass+1);
+                }
+             }
+            }
+    }
